0230852aca0e12b8486f8cad5895c0e8012c667c
Qquehue <alejandro.qquehue@sptech.school>
2022-05-11 22:03:43 -0300

    atualizacao

---
+++ script_Palavras_Cruzadas.js
@@ -0,0 +1,64 @@
+  function confirmar() {
+    var res1 = ipt_r1.value;
+    var res2 = ipt_r2.value;
+
+    if (res1 == "processador") {
+      var processador = document.querySelector('#cpu1')
+      processador.style.display = "block";
+      var processador = document.querySelector('#cpu2')
+      processador.style.display = "block";
+      var processador = document.querySelector('#cpu3')
+      processador.style.display = "block";
+      var processador = document.querySelector('#cpu4')
+      processador.style.display = "block";
+      var processador = document.querySelector('#cpu5')
+      processador.style.display = "block";
+      var processador = document.querySelector('#cpu6')
+      processador.style.display = "block";
+      var processador = document.querySelector('#cpu7')
+      processador.style.display = "block";
+      var processador = document.querySelector('#cpu8')
+      processador.style.display = "block";
+      var processador = document.querySelector('#cpu9')
+      processador.style.display = "block";
+      var processador = document.querySelector('#cpu10')
+      processador.style.display = "block";
+      var processador = document.querySelector('#cpu11')
+      processador.style.display = "block";
+    }
+    if (res2 == "circuito digital"){
+      var circuito = document.querySelector('#circuito_digital1')
+      circuito.style.display = "block";
+      var circuito = document.querySelector('#circuito_digital2')
+      circuito.style.display = "block";
+      var circuito = document.querySelector('#circuito_digital3')
+      circuito.style.display = "block";
+      var circuito = document.querySelector('#circuito_digital4')
+      circuito.style.display = "block";
+      var circuito = document.querySelector('#circuito_digital5')
+      circuito.style.display = "block";
+      var circuito = document.querySelector('#circuito_digital6')
+      circuito.style.display = "block";
+      var circuito = document.querySelector('#circuito_digital7')
+      circuito.style.display = "block";
+      var circuito = document.querySelector('#circuito_digital8')
+      circuito.style.display = "block";
+      var circuito = document.querySelector('#circuito_digital9')
+      circuito.style.display = "block";
+      var circuito = document.querySelector('#circuito_digital10')
+      circuito.style.display = "block";
+      var circuito = document.querySelector('#circuito_digital11')
+      circuito.style.display = "block";
+      var circuito = document.querySelector('#circuito_digital12')
+      circuito.style.display = "block";
+      var circuito = document.querySelector('#circuito_digital13')
+      circuito.style.display = "block";
+      var circuito = document.querySelector('#circuito_digital14')
+      circuito.style.display = "block";
+      var circuito = document.querySelector('#circuito_digital15')
+      circuito.style.display = "block";
+    }
+    if (res3 == "armazenamento") {
+      
+    }
+  }

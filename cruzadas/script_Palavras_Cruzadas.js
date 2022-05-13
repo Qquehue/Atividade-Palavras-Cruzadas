27ebf6537b4c24270d402bf165d042f15d113d3a
GabrielaKubo <gabriela.kubo@sptech.school>
2022-05-12 21:05:59 -0300

    botão encerrar

--- cruzadas/script_Palavras_Cruzadas.js
+++ cruzadas/script_Palavras_Cruzadas.js
@@ -1,3 +1,4 @@
+var pont = 0;
 function confirmar() {
   var res1 = ipt_r1.value;
   var res2 = ipt_r2.value;
@@ -21,131 +22,164 @@ function confirmar() {
   var res20 = ipt_r20.value;
   var res21 = ipt_r21.value;
   
-
   if (res1 == "processador") {
+    pont ++;
     for (let contador = 1; contador <= 11; contador++) {
       var proc = document.querySelector(`#cpu${contador}`)
       proc.style.display = "block";
     }
   }
   if (res2 == "circuito digital"){
+    pont ++;
     for (let contador = 1; contador <= 15; contador++) {
       var circ = document.querySelector(`#circuito_digital${contador}`)
       circ.style.display = "block";
     }
   }
   if (res3 == "armazenamento") {
+    pont ++;
     for (let contador = 1; contador <= 13; contador++) {
       var armaz = document.querySelector(`#armazena${contador}`)
       armaz.style.display = "block";
     }
   }
   if (res4 == 'reformular') {
+    pont ++;
     for (let contador = 1; contador <= 10; contador++) {
       var ref = document.querySelector(`#refor${contador}`)
       ref.style.display = "block";
     }
   }
   if (res5 == 'volatil' || res5 == 'volátil') {
+    pont ++;
     for (let contador = 1; contador <= 7; contador++) {
       var circ = document.querySelector(`#volatil${contador}`)
       circ.style.display = "block";
     }
   }
   if (res6 == 'permanente') { 
+    pont ++;
     for (let contador = 1; contador <= 10; contador++) {
       var perma = document.querySelector(`#permanente${contador}`)
       perma.style.display = "block";
     }
   }
   if (res7 == 'leitura') {
+    pont ++;
     for (let contador = 1; contador <= 7; contador++) {
       var leitur = document.querySelector(`#leitura${contador}`)
       leitur.style.display = "block";
     }
   }
   if (res8 == 'reescrever') {
+    pont ++;
     for (let contador = 1; contador <= 10; contador++) {
       var reesc = document.querySelector(`#resscrever${contador}`)
       reesc.style.display = "block";
     }
   }
   if (res9 == 'nao volatil' || res9 == "não volátil") {
+    pont ++;
     for (let contador = 1; contador <= 10; contador++) {
       var naovol = document.querySelector(`#nao_volatil${contador}`)
       naovol.style.display = "block";
     }
   }
   if (res10 == 'convencional') {
+    pont ++;
     for (let contador = 1; contador <= 12; contador++) {
       var convenc = document.querySelector(`#convencional${contador}`)
       convenc.style.display = "block";
     }
   }
   if (res11 == 'constante') {
+    pont ++;
     for (let contador = 1; contador <= 10; contador++) {
       var permane = document.querySelector(`#permanente${contador}`)
       permane.style.display = "block";
     }
   }
   if (res12 == 'armazenar') {
+    pont ++;
     for (let contador = 1; contador <= 9; contador++) {
       var armazena = document.querySelector(`#armazenar${contador}`)
       armazena.style.display = "block";
     }
   }
   if (res13 == 'auxiliar') {
+    pont ++;
     for (let contador = 1; contador <= 8; contador++) {
       var auxi = document.querySelector(`#auxiliar${contador}`)
       auxi.style.display = "block";
+
     }
   }
   if (res14 == 'acesso direto') {
+    pont ++;
     for (let contador = 1; contador <= 12; contador++) {
       var acess = document.querySelector(`#acesso_direto${contador}`)
       acess.style.display = "block";
+
     }
   }
   if (res15 == 'intermediador') {
+    pont ++;
     for (let contador = 1; contador <= 13; contador++) {
       var interm = document.querySelector(`#intermediador${contador}`)
       interm.style.display = "block";
+
     }
   }
   if (res16 == 'endereço') {
+    pont ++;
     for (let contador = 1; contador <= 8; contador++) {
       var ende = document.querySelector(`#endereco${contador}`)
       ende.style.display = "block";
+
     }
   }
   if (res17 == 'transporte') {
+    pont ++;
     for (let contador = 1; contador <= 9; contador++) {
       var transp = document.querySelector(`#transporte${contador}`)
       transp.style.display = "block";
+
     }
   }
   if (res18 == 'quatro') {
+    pont ++;
     for (let contador = 1; contador <= 6; contador++) {
       var quat = document.querySelector(`#quatro${contador}`)
       quat.style.display = "block";
+
     }
   }
   if (res19 == 'oito') {
+    pont ++;
     for (let contador = 1; contador <= 4; contador++) {
       var oito = document.querySelector(`#oito${contador}`)
       oito.style.display = "block";
+
     }
   }
   if (res20 == 'hyper thread') {
+    pont ++;
     for (let contador = 1; contador <= 11; contador++) {
       var hype = document.querySelector(`#hyper_thread${contador}`)
       hype.style.display = "block";
+
     }
   }
   if (res21 == 'multi nucleo' || res21 == 'multi núcleo') {
+    pont ++;
     for (let contador = 1; contador <= 11; contador++) {
       var multin = document.querySelector(`#multi_nucleo${contador}`)
       multin.style.display = "block";
+
     }
   }
+}
+
+function encerrar(){
+    alert(`✨Sua pontuação final foi de ${pont}✨!!!`)
 }
